8279d1d4f60d15c866e8944a2a084b8ab53ad0d1
Igor Shturmov <shturmik@gmail.com>
2020-02-22 20:55:25 +0200

    actions

---
+++ src/components/Actions/Actions.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Actions.scss';
+
+export class Actions extends Component {
+    state = {
+        inputedMonth: new Date().getMonth(),
+        inputedYear: new Date().getFullYear(),
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            inputedYear: Number(event.target.value),
+        })
+    }
+
+    handleEnter = (event) => {
+        const { inputedYear } = this.state;
+        if (event.key === 'Enter' ) {
+            this.props.changeYear(inputedYear);
+        }
+    }
+
+    handleMonthChange = (e) => {
+        const { inputedMonth } = this.state;
+        this.setState({
+            inputedMonth: Number(e.target.value),
+        })
+        this.props.changeMonth(inputedMonth);
+    }
+
+    render() {
+        const { inputedYear } = this.state;
+        const { monthes, month } = this.props;
+
+        return(
+            <div className="action">
+              <select value={month} onChange={this.handleMonthChange} className="action__select">
+              {monthes.map((month, index) => (
+                  <option value={index+1}>{month}</option>
+                ))}
+              </select>
+            <input type='number' value={inputedYear} onChange={this.handleChange} onKeyDown={this.handleEnter} className="action__input"/>
+            </div>
+            
+        );
+    }
+}
