0c86ef4932a33d78d0234937a51f16e50f0986cd
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-22 23:15:07 +0200

    select veiws

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Calendar from './components/Calendar/Calendar';
+import { Veiw } from './components/View/View';
 
 const weakDays = ['mon', 'tue', 'wed', 'thir', 'fri', 'sat', 'sun'];
 const monthes = [
   'January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December',
 ];
+const selectBtn = ['Day', 'Week', 'Month', 'Year'];
 
 const dateNow = new Date();
 
@@ -14,18 +15,33 @@ export default class App extends React.Component {
   state = {
     month: dateNow.getMonth() + 1,
     year: dateNow.getFullYear(),
+    selectVeiw: 'Day',
+  }
+
+  selected = (event) => {
+    const veiw = event.target.value;
+
+    this.setState({
+      selectVeiw: veiw,
+    });
   }
 
   render() {
-    const { month, year } = this.state;
+    const { month, year, selectVeiw } = this.state;
 
     return (
       <div className="App">
-        <Calendar
+        <select onChange={this.selected}>
+          {selectBtn.map(
+            select => <option value={select} key={select}>{select}</option>,
+          )}
+        </select>
+        <Veiw
           month={month}
           year={year}
           weakDays={weakDays}
           monthes={monthes}
+          selectVeiw={selectVeiw}
         />
       </div>
 
