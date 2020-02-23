303be9dc739ff8dd556adcc90a5eff25a9b81fa4
Igor Shturmov <shturmik@gmail.com>
2020-02-23 02:49:26 +0200

    Updates

---
+++ precache-manifest.dfa4ec3c5f12f67818c3dead8d8e684e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c1d0cab86a134cfbf9e2fea9f0ade0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6cec04d7469a8ea99e6d",
+    "url": "./static/css/main.67796ee5.chunk.css"
+  },
+  {
+    "revision": "0e32d5d4b8f888af7995",
+    "url": "./static/js/2.591daf09.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.591daf09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cec04d7469a8ea99e6d",
+    "url": "./static/js/main.de58146d.chunk.js"
+  },
+  {
+    "revision": "b053f945e8ed7d36e27e",
+    "url": "./static/js/runtime-main.ed8c18d9.js"
+  }
+]);
