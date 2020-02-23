699ce4c9d983072428b3d97d3281cef77ee4a1c5
Igor Shturmov <shturmik@gmail.com>
2020-02-23 09:15:33 +0200

    dell files

--- public/push-worker.js
+++
@@ -1,6 +0,0 @@
-self[`appKey`] = `ab812fc071ddb70dba84b596df73cc48`;
-self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
-self.importScripts(`${self[`hostUrl`]}/worker.js`);
-// uncomment and set path to your service worker
-// if you have one with precaching functionality (has oninstall, onactivate event listeners)
-// self.importScripts('path-to-your-sw-with-precaching')
